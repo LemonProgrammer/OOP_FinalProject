41f0b46fa382cc0238f9445f251d908752f37ee6
Fernando Blanco <fblanco@student.neumont.edu>
2018-06-03 18:43:03 -0600

    re-added

---
+++ Galamont/src/models/EmptyTile.java
@@ -0,0 +1,7 @@
+package models;
+
+public class EmptyTile extends GameModels {
+	public EmptyTile() {
+		super("EMT");
+	}
+}
---
+++ GalamontTest/src/models/Block.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Block extends ModelComponent {
+
+private boolean isDestroyable;
+
+public boolean isDestroyable() {
+	return isDestroyable;
+}
+
+public void setDestroyable(boolean isDestroyable) {
+	
+	this.isDestroyable = isDestroyable;
+}
+
+	public Block(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean checkIfIsShowable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setShowability(boolean canShow) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	}
